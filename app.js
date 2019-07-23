2c8119a4176b66c3ebccee573b27fe5b7095f0a2
asusual1540 <mehedihasanadnan@gmail.com>
2019-07-24 03:05:48 +0600

    Fixed bug

--- app.js
+++ app.js
@@ -11,19 +11,17 @@ const isAuth = require("./middleware/is-auth");
 const app = express();
 const port = process.env.PORT || 4000;
 
-console.log(process.env)
-
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 
 
 
